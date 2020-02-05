49ea4fa7e6627bb6a23951a9b85b4c10f9cea0db
Jonas Blixt <jonpe960@gmail.com>
2020-02-05 10:23:50 +0100

    Complete the compare function

--- src/compare.c
+++ src/compare.c
@@ -25,6 +25,8 @@ int action_compare(int argc, char **argv)
     int long_index = 0;
     const char *filename1 = NULL;
     const char *filename2 = NULL;
+    uint8_t chunk1[4096];
+    uint8_t chunk2[4096];
 
     struct option long_options[] =
     {
@@ -144,7 +146,7 @@ int action_compare(int argc, char **argv)
         bpak_get_meta(h1p, m->id, (void **) &data1);
 
         rc = bpak_get_meta(h2p, m->id, (void **) &data2);
-        
+
         /* Missing in file 2? */
         if (rc != BPAK_OK)
             removed = true;
@@ -195,7 +197,7 @@ int action_compare(int argc, char **argv)
         bpak_get_meta(h2p, m->id, (void **) &data1);
 
         rc = bpak_get_meta(h1p, m->id, (void **) &data2);
-        
+
         /* Missing in file 2? */
         if (rc != BPAK_OK)
         {
@@ -207,6 +209,144 @@ int action_compare(int argc, char **argv)
         }
     }
 
+    /* Compare parts */
+
+    printf("\nParts:\n");
+    printf("    ID         Size         Z-pad  Flags          Transport Size\n");
+
+    char flags_str[9] = "--------";
+    struct bpak_part_header *p2 = NULL;
+    struct bpak_part_header *p1 = NULL;
+
+    bpak_foreach_part(h1p, p)
+    {
+        change = false;
+        removed = false;
+
+        if (p->id)
+        {
+            rc = bpak_get_part(h2p, p->id, &p2);
+
+            if (rc != BPAK_OK)
+            {
+                removed = true;
+            }
+            else
+            {
+                /* Check if metadata is the same */
+                if (memcmp(p2, p, sizeof(*p)) != 0)
+                    change = true;
+
+                /* Compare the data */
+                rc = bpak_io_seek(io1, bpak_part_offset(h1p, p), BPAK_IO_SEEK_SET);
+
+                if (rc != BPAK_OK)
+                {
+                    printf("Error: Seek failed\n");
+                    break;
+                }
+
+                rc = bpak_io_seek(io2, bpak_part_offset(h2p, p2), BPAK_IO_SEEK_SET);
+
+                if (rc != BPAK_OK)
+                {
+                    printf("Error: Seek failed\n");
+                    break;
+                }
+
+                size_t data_to_compare = bpak_part_size(p);
+
+                while (data_to_compare)
+                {
+                    size_t chunk = (data_to_compare > sizeof(chunk1)) ? \
+                                   sizeof(chunk1):data_to_compare;
+
+                    bpak_io_read(io1, chunk1, chunk);
+                    bpak_io_read(io2, chunk2, chunk);
+
+                    if (memcmp(chunk1, chunk2, chunk) != 0)
+                    {
+                        change = true;
+                        break;
+                    }
+
+                    data_to_compare -= chunk;
+                }
+            }
+
+            if (change)
+            {
+                printf(RED_CLR);
+                printf("*");
+            }
+            else if(removed)
+            {
+                printf(RED_YL);
+                printf("-");
+            }
+            else
+            {
+                printf("=");
+            }
+
+            if (p->flags & BPAK_FLAG_EXCLUDE_FROM_HASH)
+                flags_str[0] = 'h';
+            else
+                flags_str[0] = '-';
+
+            if (p->flags & BPAK_FLAG_TRANSPORT)
+                flags_str[1] = 'T';
+            else
+                flags_str[1] = '-';
+
+            printf("   %8.8x   %-12lu %-3u    %s",p->id, p->size, p->pad_bytes,
+                                                flags_str);
+
+            if (p->flags & BPAK_FLAG_TRANSPORT)
+                printf("       %-12lu", p->transport_size);
+            else
+                printf("       %-12lu", p->size);
+
+            printf("\n");
+            printf(NO_CLR);
+        }
+    }
+
+    /* Check parts for parts in 2 that are missing in 1 */
+    bpak_foreach_part(h2p, p)
+    {
+        if (p->id)
+        {
+            rc = bpak_get_part(h1p, p->id, &p1);
+
+            if (rc != BPAK_OK)
+            {
+                printf(RED_YL);
+                printf("-");
+
+                if (p->flags & BPAK_FLAG_EXCLUDE_FROM_HASH)
+                    flags_str[0] = 'h';
+                else
+                    flags_str[0] = '-';
+
+                if (p->flags & BPAK_FLAG_TRANSPORT)
+                    flags_str[1] = 'T';
+                else
+                    flags_str[1] = '-';
+
+                printf("   %8.8x   %-12lu %-3u    %s",p->id, p->size, p->pad_bytes,
+                                                    flags_str);
+
+                if (p->flags & BPAK_FLAG_TRANSPORT)
+                    printf("       %-12lu", p->transport_size);
+                else
+                    printf("       %-12lu", p->size);
+
+                printf("\n");
+                printf(NO_CLR);
+            }
+        }
+    }
 err_close_io1_out:
     bpak_io_close(io1);
 err_close_io2_out:
