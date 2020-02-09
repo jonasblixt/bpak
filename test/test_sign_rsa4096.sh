15073a13fcda0debb3e285d75bec87830bb9c2c6
Jonas Blixt <jonpe960@gmail.com>
2020-02-09 15:48:32 +0100

    Add test for rsa4096

---
+++ test/test_sign_rsa4096.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+BPAK=../src/bpak
+echo Sign test rsa4096
+set -e
+V=-vvv
+
+IMG=sign_test_rsa4096.bpak
+PKG_UUID=0888b0fa-9c48-4524-9845-06a641b61edd
+PKG_UNIQUE_ID_A=$(uuidgen -t)
+set -e
+
+# Create A package
+$BPAK create $IMG -Y --hash-kind sha256 --signature-kind rsa4096 $V
+
+$BPAK add $IMG --meta bpak-package --from-string $PKG_UUID --encoder uuid $V
+$BPAK add $IMG --meta bpak-package-uid --from-string $PKG_UNIQUE_ID_A \
+                 --encoder uuid $V
+
+$BPAK sign $IMG --key $srcdir/dev_rsa_private.pem \
+                  --key-id pb-development \
+                  --key-store pb-internal $V
+
+$BPAK show $IMG $V
+$BPAK verify $IMG --key $srcdir/dev_rsa_public.der $V
