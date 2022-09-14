f17cba4f6d6223e93adda2bb81dcd1ac67b503f3
Jonas Blixt <jonpe960@gmail.com>
2022-09-14 20:48:03 +0200

    test: Improve coverage on bpak core

---
+++ test/test_misc.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <bpak/bpak.h>
+#include <bpak/utils.h>
+#include <bpak/id.h>
+#include "nala.h"
+
+TEST(error_codes)
+{
+    unsigned int end = BPAK_KEY_NOT_FOUND;
+
+    for (unsigned int i = 0; i < end; i++) {
+        ASSERT(bpak_error_string(-i) != bpak_error_string(1));
+    }
+
+    printf("%s %s\n", bpak_error_string(end+1), bpak_error_string(1));
+    ASSERT(bpak_error_string(end+1) == bpak_error_string(1));
+}
