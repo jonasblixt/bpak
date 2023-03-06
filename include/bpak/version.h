f57d0e059d00cff7390a74701b71c2e83ed18be9
Jonas Blixt <jonpe960@gmail.com>
2023-03-06 16:26:02 +0100

    Version 0.9.2

--- include/bpak/version.h
+++ include/bpak/version.h
@@ -3,7 +3,7 @@
 
 #define BPAK_VERSION_MAJOR  0
 #define BPAK_VERSION_MINOR  9
-#define BPAK_VERSION_PATCH  1
-#define BPAK_VERSION_STRING "0.9.1"
+#define BPAK_VERSION_PATCH  2
+#define BPAK_VERSION_STRING "0.9.2"
 
 #endif
